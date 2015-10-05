0c901ced832f118fe368bd9e1ffce7940ed53c5a
Carson Logan <carsonklogan@gmail.com>
2015-10-04 17:44:38 -0700

    hover transitions improved for card classes

--- src/components/AppointmentViewer/PracticeList/PracticeCard/PractitionerCard/PractitionerCard.js
+++ src/components/AppointmentViewer/PracticeList/PracticeCard/PractitionerCard/PractitionerCard.js
@@ -16,6 +16,7 @@ export default class PractitionerCard extends Component {
         position: 'relative',
         width: '480px',
         boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+        transitionDuration: '0.3s',
         ':hover': {
           boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
         },
